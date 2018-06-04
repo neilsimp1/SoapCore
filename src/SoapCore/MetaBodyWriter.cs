6f242e0c17b9081b2e0534cd992f6f62a9735835
Simpson <Neil.Simpson@its.ny.gov>
2018-06-04 13:19:46 -0400

    Add Guid type to MetaBodyWriter

--- src/SoapCore/MetaBodyWriter.cs
+++ src/SoapCore/MetaBodyWriter.cs
@@ -433,6 +433,9 @@ namespace SoapCore
 				case "DateTime":
 					resolvedType = "xs:dateTime";
 					break;
+				case "Guid":
+					resolvedType = "xs:string";
+					break;
 			}
 
 			if (String.IsNullOrEmpty(resolvedType))
